a552ce331263a9dc004dd2d64fe660d8c48c65ca
jamilsonjr <jamilsonpinheiro.jr@gmail.com>
2022-06-09 18:33:11 +0100

    gb intro added

--- thesis_package/aimodels.py
+++ thesis_package/aimodels.py
@@ -3,12 +3,11 @@ from abc import ABC, abstractmethod
 # Implement the Stretegy design pattern.
 class Strategy(ABC):
     @abstractmethod
-    def do_algorithm(self, data: dict) -> None:
-        pass    
-class Strategy_1(Strategy):
-    def do_algorithm(self, data: dict) -> None:
-        print("Strategy 1: ", data)
-        return data * 100
+    def fit(self, data: dict) -> None:
+        pass
+    @abstractmethod
+    def predict(self, data: dict) -> None:
+        pass
 class Context():
     def __init__(self, strategy: Strategy) -> None:
         self._strategy = strategy
@@ -17,7 +16,29 @@ class Context():
         return self._strategy
     @strategy.setter
     def strategy(self, strategy: Strategy) -> None:
-        self._strategy = strategy
-    def do_algorithm(self, data: dict) -> None:
-        return self._strategy.do_algorithm(data)
-        
+       self._strategy = strategy
+    def fit(self, data: dict) -> None:
+        return self._strategy.fit(data)
+    def predict(self, data: dict) -> None:
+        return self._strategy.predict(data)
+
+# Implement the Linear Regression Strategy Class using sci-kit learn.
+from sklearn.linear_model import LinearRegression
+class LinearRegressionStrategy(Strategy):
+    def __init__(self) -> None:
+        self.model = LinearRegression()
+    def fit(self, data: dict) -> None:
+        self.model.fit(data['X_train'], data['y_train'])
+    def predict(self, data: dict) -> None:
+        return self.model.predict(data['X_test'])
+
+# Implement Multi Output Gradient Boost Regressor Strategy Class using sklearn.
+from sklearn import ensemble
+from sklearn.multioutput import MultiOutputRegressor
+class GradientBoostRegressorStrategy(Strategy):
+    def __init__(self, hyper_parms: dict) -> None:
+        self.model =  MultiOutputRegressor(ensemble.GradientBoostingRegressor(**hyper_parms))
+    def fit(self, data: dict) -> None:
+        self.model.fit(data['X_train'], data['y_train'])
+    def predict(self, data: dict) -> None:
+        return self.model.predict(data['X_test'])
